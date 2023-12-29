c9b90f0de66719bdd51392e1c93df7642c6d15a5
Dmitriy Starokozhev <p1cachu@yandex.ru>
2023-12-29 20:18:34 +0300

    Файлы Unity проекта

---
+++ Assets/Game.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public Transform[] _gameObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError("OOps");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Message for students");
+    }
+}
